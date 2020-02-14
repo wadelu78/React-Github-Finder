f4d96eaa0e8fbded814540b6be9cb7600d1ac6c7
Wei Lu <wadelu0714@gmail.com>
2020-02-14 22:17:50 +1300

    initial commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+
+export class Navbar extends Component {
+  static defaultProps = {
+    title: "Github Finder",
+    icon: "fab fa-github"
+  }
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  }
+  render() {
+    return (
+      <nav className="navbar bg-primary">
+        <h1>
+          <i className={this.props.icon}></i> {this.props.title}
+        </h1>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
+
+//1. for a class based component, use {this.props.title}
+//title comes from <Navbar title="Github Finder" /> in the App.js
+
+//2. static defaultProps = {
+//    title: "Github Finder",
+//    icon: "fab fa-github"
+//  }
+// this is the defaultProps
+
+//3. Type checking propTypes
